184b3c83f2c9715f18dbb3f253744bf7eccd3ee5
ombima56 <ombimahillary6@gmail.com>
2024-08-26 12:49:58 +0300

    feat: add colons

--- declarations.js
+++ declarations.js
@@ -1,21 +1,22 @@
-const escapeStr = '`\\/"\''
-const arr = [4, '2']
+const escapeStr = '`\\/"\'';
+const arr = [4, '2'];
 const obj = {
     str : 'string',
     num : 8,
     bool: false,
     undef: undefined,
-}
+};
 const nested = {
     arr : [4, undefined, '2'],
     obj : {
         str: 'string',
         num : 9,
-        bool: false,
+        bool: false
     }
-}
+};
 
-Object.freeze(arr)
-Object.freeze(obj)
-Object.freeze(nested.arr)
-Object.freeze(nested.obj)
+Object.freeze(nested.arr);
+Object.freeze(nested.obj);
+Object.freeze(nested);
+Object.freeze(obj);
+Object.freeze(arr);
