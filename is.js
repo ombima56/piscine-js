734e54eaf9456b80e3b9318aba9c16009281bb17
ombima56 <ombimahillary6@gmail.com>
2024-08-27 14:44:03 +0300

    Add is that a function properties to the is object to check value types. Each function should take one argument, and return a boolean

---
+++ is.js
@@ -0,0 +1,13 @@
+const is = {
+    num: value => typeof value === 'number' && !isNaN(value),
+    nan: value => typeof value === 'number' && isNaN(value),
+    str: value => typeof value === 'string',
+    bool: value => typeof value === 'boolean',
+    undef: value => typeof value === 'undefined',
+    def: value => typeof value !== 'undefined',
+    arr: value => Array.isArray(value),
+    obj: value => value !== null && typeof value === 'object' && !Array.isArray(value),
+    fun: value => typeof value === 'function',
+    truthy: value => !!value,
+    falsy: value => !value
+};
