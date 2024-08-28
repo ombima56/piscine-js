475bcdf3fe711a5d18a3fc4539fc787442cb236a
ombima56 <ombimahillary6@gmail.com>
2024-08-29 01:00:31 +0300

    chore: update a loop to incrementally build up the integer part of the number. This avoids using any bitwise operations

--- cut-corners.js
+++ cut-corners.js
@@ -1,28 +1,40 @@
 function getIntegerPart(num) {
+    let sign = num >= 0 ? 1 : -1;
+    num = Math.abs(num);
     let result = 0;
-    while (Math.abs(result) <= Math.abs(num)) {
-        result += num > 0 ? 1 : -1;
+    let place = 1;
+    
+    while (place <= num) {
+        place *= 10;
     }
-    return result - (num > 0 ? 1 : -1);
+    place /= 10;
+
+    while (place >= 1) {
+        while (result + place <= num) {
+            result += place;
+        }
+        place /= 10;
+    }
+
+    return sign * result;
 }
 
 function round(num) {
-    const intPart = getIntegerPart(num);
-    const decPart = num - intPart;
+    let intPart = getIntegerPart(num);
+    let decPart = num - intPart;
     if (Math.abs(decPart) >= 0.5) {
         return intPart + (num >= 0 ? 1 : -1);
-    } else {
-        return intPart;
     }
+    return intPart;
 }
 
 function ceil(num) {
-    const intPart = getIntegerPart(num);
+    let intPart = getIntegerPart(num);
     return num > intPart ? intPart + 1 : intPart;
 }
 
 function floor(num) {
-    const intPart = getIntegerPart(num);
+    let intPart = getIntegerPart(num);
     return num < intPart ? intPart - 1 : intPart;
 }
 
