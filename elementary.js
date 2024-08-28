1ca67ac5cc514459c380e5bca06c7ab0527621fb
ombima56 <ombimahillary6@gmail.com>
2024-08-28 17:34:14 +0300

    Add solution to multiply, divid and modulo without using there operations

---
+++ elementary.js
@@ -0,0 +1,57 @@
+function multiply(a, b) {
+    let mult = 0;
+    for (let i = 0; i < b; i++) {
+        mult += a;
+    }
+    return mult;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+
+    let quotient = 0;
+    let isNegative = false;
+
+    // Handle negative numbers
+    if (a < 0 && b > 0) {
+        a = -a;
+        isNegative = true;
+    } else if (a > 0 && b < 0) {
+        b = -b;
+        isNegative = true;
+    } else if (a < 0 && b < 0) {
+        a = -a;
+        b = -b;
+    }
+
+    while (a >= b) {
+        a -= b;
+        quotient++;
+    }
+    return isNegative ? -quotient: quotient;
+}
+
+function modulo(a,b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+
+    let isNegative = false;
+
+    if (a < 0) {
+        a = -a;
+        isNegative = true;
+    }
+
+    if (b < 0) {
+        b = -b;
+    }
+
+    while (a >= b) {
+        a -= b;
+    }
+
+    return isNegative ? -a : a;
+}
