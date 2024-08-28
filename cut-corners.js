f371065ca59d74bf2bc5d6a51a06283a4910a65e
ombima56 <ombimahillary6@gmail.com>
2024-08-29 00:54:07 +0300

    chore: Add function getIntegerPart that  uses the double bitwise NOT (~~) operator to get the integer part of a number.

--- cut-corners.js
+++ cut-corners.js
@@ -1,37 +1,27 @@
-function round(num) {
-    const intPart = num < 0 ? -(-num | 0) : (num | 0);
-    const decPart = num - intPart; // Get the decimal part
+function getIntegerPart(num) {
+    return num >= 0 ? ~~num : -~~(-num);
+}
 
-    // If decimal part is 0.5 or more, round up
-    if (decPart >= 0.5) {
-        return intPart + 1;
+function round(num) {
+    const intPart = getIntegerPart(num);
+    const decPart = num - intPart;
+    if (Math.abs(decPart) >= 0.5) {
+        return intPart + (num >= 0 ? 1 : -1);
     } else {
         return intPart;
     }
 }
 
 function ceil(num) {
-    const intPart = num | 0;
-    if (num === intPart) {
-        return intPart;
-    }
-
-    // For positive numbers or non-integers, return intPart + 1
-    return num > 0 ? intPart + 1 : intPart;
+    const intPart = getIntegerPart(num);
+    return num > intPart ? intPart + 1 : intPart;
 }
 
 function floor(num) {
-    const intPart = num | 0; // Get the integer part
-
-    // If num is already an integer, return it
-    if (num === intPart) {
-        return intPart; 
-    }
-
-    // For positive numbers, return the integer part
-    return num > 0 ? intPart : intPart - 1;
+    const intPart = getIntegerPart(num);
+    return num < intPart ? intPart - 1 : intPart;
 }
 
 function trunc(num) {
-    return num < 0 ? -(-num | 0) : (num | 0);
+    return getIntegerPart(num);
 }
