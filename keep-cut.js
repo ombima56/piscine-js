21b67af1dcf2c49081617e1fe3a7c641f9f6e82b
ombima56 <ombimahillary6@gmail.com>
2024-08-28 23:01:16 +0300

    solution to functions, which each take a string as an argument

---
+++ keep-cut.js
@@ -0,0 +1,55 @@
+function cutFirst(str) {
+    let result = "";
+    for (let i = 2; i < str.length; i++) {
+        result += str[i];
+    }
+    return result;
+}
+
+function cutLast(str) {
+    let result = "";
+    for (let i = 0; i < str.length-2; i++) {
+        result += str[i];
+    }
+    return result;
+}
+
+function cutFirstLast(str) {
+    let result = "";
+    for (i = 2; i < str.length-2;i++) {
+        result += str[i];
+    }
+    return result;
+}
+
+function keepFirst(str) {
+    let result = '';
+    for (let i = 0; i < 2 && i < str.length; i++) {
+        result += str[i];
+    }
+    return result;
+}
+
+function keepLast(str) {
+    let result = '';
+    for (let i = str.length-2; i < str.length; i++) {
+        result += str[i];
+    }
+    return result;
+}
+
+function keepFirstLast(str){
+    let result = '';
+    let length = str.length;
+
+    for (let i = 0; i < length; i++) {
+        if (i < 2) {
+            result += str[i];
+        }
+        if (i >= length - 2 && length > 2) {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
