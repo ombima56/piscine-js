d5324477dcc662d8dd8f54ac927c91ba9bf36016
ombima56 <ombimahillary6@gmail.com>
2024-08-29 00:42:14 +0300

    refactor: remove bitwise operator

--- cut-corners.js
+++ cut-corners.js
@@ -1,7 +1,8 @@
 function round(num) {
     const intPart = num < 0 ? -(-num | 0) : (num | 0);
-    const decPart = num - intPart;
+    const decPart = num - intPart; // Get the decimal part
 
+    // If decimal part is 0.5 or more, round up
     if (decPart >= 0.5) {
         return intPart + 1;
     } else {
@@ -10,29 +11,27 @@ function round(num) {
 }
 
 function ceil(num) {
-    // if the number is an already integer
-    if (num === (num | 0)) {
-        return num;
+    const intPart = num | 0;
+    if (num === intPart) {
+        return intPart;
     }
 
-    if (num > 0) {
-        return (num | 0) + 1;
-    }
-    return num | 0;
+    // For positive numbers or non-integers, return intPart + 1
+    return num > 0 ? intPart + 1 : intPart;
 }
 
-function floor(num){
-    if (num === (num | 0)) {
-        return num;
+function floor(num) {
+    const intPart = num | 0; // Get the integer part
+
+    // If num is already an integer, return it
+    if (num === intPart) {
+        return intPart; 
     }
 
-    if (num > 0) {
-        return num | 0;
-    }
-
-    return (num | 0) - 1;
+    // For positive numbers, return the integer part
+    return num > 0 ? intPart : intPart - 1;
 }
 
 function trunc(num) {
-    return (num > 0 ? num | 0 : (num | 0)+ (num < 0 ? 0 : 0));
+    return num < 0 ? -(-num | 0) : (num | 0);
 }
