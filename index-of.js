89ca37f75afbf232c10af69e359d64fff5d838f5
ombima56 <ombimahillary6@gmail.com>
2024-08-28 21:45:37 +0300

    fix: fix indeOf function to start from the first index

--- index-of.js
+++ index-of.js
@@ -1,5 +1,5 @@
-function indexOf(arr, char) {
-    for( let i = 0; i <= arr.length;i++) {
+function indexOf(arr, char, start = 0) {
+    for( let i = start; i < arr.length;i++) {
         if (arr[i] === char) {
             return i;
         }
