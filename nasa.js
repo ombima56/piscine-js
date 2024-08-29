193ea7d89a016c68f7655936b0e80a7f05eb0760
ombima56 <ombimahillary6@gmail.com>
2024-08-29 15:19:55 +0300

    Add solution to a function named nasa that takes a number N as an argument and returns a string with all numbers from 1 to N separated by spaces.

---
+++ nasa.js
@@ -0,0 +1,15 @@
+function nasa(N){
+    let result = [];
+    for (let i= 1; i <= N;i++) {
+        if (i%3 === 0) {
+            result.push("NA");
+        } else if (i%5 === 0) {
+            result.push("SA");
+        } else if (i%3 === 0 && i%5 === 0){
+            result.push("NASA");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result.join(" ");
+}
