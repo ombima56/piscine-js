a65c09bbdc57792f28108af712dae7a46b01b939
ombima56 <ombimahillary6@gmail.com>
2024-08-29 15:29:38 +0300

    refactor: optimize modulo comparison in function

--- nasa.js
+++ nasa.js
@@ -1,12 +1,12 @@
-function nasa(N){
+function nasa(N) {
     let result = [];
-    for (let i= 1; i <= N;i++) {
-        if (i%3 === 0) {
-            result.push("NA");
-        } else if (i%5 === 0) {
-            result.push("SA");
-        } else if (i%3 === 0 && i%5 === 0){
+    for (let i = 1; i <= N; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
             result.push("NASA");
+        } else if (i % 3 === 0) {
+            result.push("NA");
+        } else if (i % 5 === 0) {
+            result.push("SA");
         } else {
             result.push(i.toString());
         }
