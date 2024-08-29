577691fd0dfd4c8c43b1d68b347cd9672d9a5484
ombima56 <ombimahillary6@gmail.com>
2024-08-29 17:38:01 +0300

    refactor: remove the redudant line

--- get.js
+++ get.js
@@ -2,18 +2,17 @@ function get(src, path){
     const keys = path.split('.')
     let current = src;
 
+    
+
     for (let key of keys) {
-        if (current.hasOwnProperty(key)){
-            if (Array.isArray(current) && !isNaN(key) && Number.isInteger(Number(key))) {
-                current = current[parseInt(key,10)]
-            } else {
-                current = current[key];
-            }
-            
-        } else {
+        if (current === null || current === undefined) {
             return undefined;
         }
+        if (Array.isArray(current) && !isNaN(key) && Number.isInteger(Number(key))) {
+            current = current[parseInt(key,10)]
+        } else {
+            current = current[key];
+        }
     }
-
     return current;
 }
