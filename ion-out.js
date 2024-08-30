9407faed62a1f98eec355c7786e344f1b8ea353b
ombima56 <ombimahillary6@gmail.com>
2024-08-30 23:17:14 +0300

    feat: add functionality to remove trailing spaces from user input

--- ion-out.js
+++ ion-out.js
@@ -1,10 +1,11 @@
 function ionOut(str) {
-    const words = str.split(/\s|,\s*/); // Split by whitespace
+    const words = str.split(/\s|,\s*/); // Split by whitespace or commas
 
     const result = words
-        .filter(word => /t.*ion/.test(word)) // Check for 'ion' following 't'
-        .map(word => word.replace(/ion$/, ''));
-            return result;
+        .filter(word => /t(?:.*?)ion$/.test(word)) // Check for 't' followed by 'ion'
+        .map(word => word.replace(/ion$/, '')); // Replace 'ion' at the end
+    
+    return result;
 }
 
 console.log(ionOut("The nation was in motion, but the attention was lacking."));
