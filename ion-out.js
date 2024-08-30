8b6bf2f1ff9c93fc073fa10356d6422bc3b353af
ombima56 <ombimahillary6@gmail.com>
2024-08-30 22:53:32 +0300

    feat: improve input handling by removing trailing spaces

--- ion-out.js
+++ ion-out.js
@@ -3,9 +3,10 @@ function ionOut(str) {
 
     const result = words
         .filter(word => /t.*ion/.test(word)) // Check for 'ion' following 't'
-        .map(word => word.replace(/ion$/, '')); // Remove 'ion' from the end of the word
+        .map(word => word.replace(/ion\b/, '').replace(/[.,!;?]$/, ''));// Remove 'ion' and trailing punctuation
 
     return result;
 }
 
 console.log(ionOut("The nation was in motion, but the attention was lacking."));
+console.log(ionOut('attention, direction'));
