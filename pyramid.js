3565a849f6221d88a55ee6fd76ae1e1895c32c1c
ombima56 <ombimahillary6@gmail.com>
2024-08-30 15:48:18 +0300

    feat: update function to check padding against current width

--- pyramid.js
+++ pyramid.js
@@ -1,13 +1,20 @@
 function pyramid(str, n) {
     let pattern = '';
-    for(let i = 1; i <= n; i++) {
+    const maxWidth = n * 2 - 1;
+
+    for (let i = 1; i <= n; i++) {
         let line = '';
-        for (let j = 0; j < n - i; j++) {
-            line +=' ';
+        const currentWidth = 2 * i - 1; 
+        const padding = Math.floor((maxWidth - currentWidth) / 2);
+
+        for (let j = 0; j < padding; j++) {
+            line += ' ';
         }
-        for (let k = 0; k < 2 * i - 1; k++) {
+
+        for (let k = 0; k < currentWidth; k++) {
             line += str;
         }
+
         pattern += line;
 
         if (i < n) {
