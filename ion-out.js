ed75bfe945c6915bd0226631b4c69563740627c6
ombima56 <ombimahillary6@gmail.com>
2024-08-30 23:58:16 +0300

    feat: improve input handling by removing trailing spaces

--- ion-out.js
+++ ion-out.js
@@ -1,12 +1,9 @@
 function ionOut(str) {
-    const words = str.split(/\s|,\s*/); // Split by whitespace or commas
-
-    const result = words
-    .filter(word => /t.*ion$/.test(word) && /t.*ion$/.exec(word)[0] === word) // Check for 't' followed by 'ion'
-        .map(word => word.replace(/ion$/, '')); // Replace 'ion' at the end
-    
-    return result;
+    const regex = /\b(\w+t)ion\b/g;
+    const matches = str.match(regex) || [];
+    return matches.map(word => word.slice(0, -3));
 }
+
 
 console.log(ionOut("The nation was in motion, but the attention was lacking."));
 console.log(ionOut('attention, direction')); 
