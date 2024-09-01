31d64abbb0aa2107ba3a0771783b041b6929a22d
ombima56 <ombimahillary6@gmail.com>
2024-09-01 10:25:58 +0300

    Add solution to a function named findIP, that returns an array of valid IP addresses from a given string. These addresses may or may not have a port.

--- valid-ip.js
+++ valid-ip.js
@@ -1,8 +1,8 @@
-function findIP(dataSet) {
+function findIP(arr) {
     // Regular expression to match valid IP addresses with optional port numbers
     const ipPattern = /\b((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(:([0-5]?[0-9]{1,4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]))?\b/g;
     
-    const matches = dataSet.match(ipPattern) || [];
+    const matches = arr.match(ipPattern) || [];
     
     return matches;
 }
