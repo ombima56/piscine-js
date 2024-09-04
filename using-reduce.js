a67c380cb2c1d6eba5c2db7d11f6790b549ff60e
ombima56 <ombimahillary6@gmail.com>
2024-09-04 12:50:52 +0300

    Add solution to three function that adder: accepts an array of numbers, and returns the sum as a number, sumOrMul: accepts an array of numbers and adds or multiplies its elements depending on whether the element is odd or even. Even = multiply. Odd = add, funcExec: accepts an array of functions and executes them using reduce, returning the result.

---
+++ using-reduce.js
@@ -0,0 +1,32 @@
+function adder(arr) {
+    return arr.reduce((sum, num) =>  sum + num, 0);
+}
+
+const numbers1 = [1, 2, 3, 4, 5];
+
+function sumOrMul(arr, initialVAlue) {
+    return arr.reduce((acc, num) => {
+        if (num % 2 === 0) {
+            return acc * num; // If the numbers are multiple
+        } else {
+            return acc + num; // If the number are odd add
+        }
+    }, initialVAlue);
+}
+
+const numbers2 = [1, 2, 3, 5, 8];
+
+function funcExec(funcArray, initialValue) {
+    return funcArray.reduce((acc, func) => func(acc), initialValue);
+}
+
+const functions = [
+    (x) => x + 1,
+    (x) => x * 2,
+    (x) => x - 3,
+];
+
+console.log(adder(numbers1));
+console.log(sumOrMul(numbers2, 5));
+console.log(funcExec(functions, 5));
+
