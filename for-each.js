de26060918dda1d93f705ddb2006888b52f68694
ombima56 <ombimahillary6@gmail.com>
2024-09-04 13:55:43 +0300

    Add solution to a function named forEach which takes an array as the first argument, a function as the second argument, and that works like the Array.prototype.forEach method.

---
+++ for-each.js
@@ -0,0 +1,14 @@
+const forEach = (arr, func) => {
+    const result = [];
+    for ( let i = 0; i < arr.length; i++) {
+        result.push(func(arr[i]));
+    }
+    return result;
+}
+
+const mult = num => {
+    return num * 2;
+}
+
+const arr = [1,2,3,4,5,]
+console.log(forEach(arr, mult))
