ff8cb886fe916e37f5b7d700719077c059d342a6
ombima56 <ombimahillary6@gmail.com>
2024-09-04 15:03:30 +0300

    Add function  a flatMap function that takes an array as the first argument, a function as second, and that works like the method .flatMap

--- mapper.js
+++ mapper.js
@@ -6,7 +6,28 @@ const map = (arr, func) => {
     return result;
 }
 
+const flatMap = (arr, func) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        const mapped = func(arr[i], i, arr); // Apply function to the element
+
+        if (Array.isArray(mapped)) {
+            // If the result is an array, concatenate its elements to result
+            result.push(...mapped);
+        } else {
+            // Otherwise, just push the result
+            result.push(mapped);
+        }
+    }
+    return result;
+}
+
 // const numbers = [1, 4, 9];
 // console.log(numbers);
 // const root = (num) => Math.sqrt(num);
 // console.log(map(numbers, root));
+
+// const arr = [1, 2, 3];
+// const func = x => [x, x * 2];
+
+// console.log(flatMap(arr, func));
