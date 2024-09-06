ac9d97bea3e4ccde2a02ae57c3658db3c6a7a7f1
ombima56 <ombimahillary6@gmail.com>
2024-09-06 14:20:12 +0300

    Add solution to functions that function, which returns an array containing 2 arrays of HTML, getClassical function, which returns an array containing 2 arrays of HTML elements:, getActive function, which returns an array containing 2 arrays of HTML elements: and getBonannoPisano function, which returns an array containing:

---
+++ get-them-all.js
@@ -0,0 +1,49 @@
+const getArchitects = () => {
+    const a_tags = document.querySelectorAll('a');
+    const span_elem = document.querySelectorAll('span')
+
+    const architects = Array.from(a_tags);
+    const nonArchitects = Array.from(span_elem);
+
+    return [architects, nonArchitects];
+}
+
+const getClassical = () => {
+    const architects = getArchitects()[0];
+    const classical_class = architects.filter(arch => {
+        arch.classList.contains('classical');
+    });
+
+    const non_classical = architects.filter(arch => {
+        !arch.classList.contains('classical')
+    });
+
+    return [classical_class, non_classical];
+}
+
+const getActive = () => {
+    const active = getClassical()[0];
+
+    const active_classical = active.filter(arch => arch.classList.contains('active'));
+
+    const non_active_classical = active.filter(arch => !arch.classList.contains('active'));
+
+    return [active_classical, non_active_classical];
+}
+
+const getBonannoPisano = () => {
+    const bonannoPisano = document.getElementById('BonannoPisano');
+
+    // Get all active classical architects
+    const activeClassicalArchitects = Array.from(document.getElementsByClassName('classical'))
+        .filter(arch => arch.classList.contains('active'));
+
+    // Filter out Bonanno Pisano from the active classical architects
+    const remainingArchitects = activeClassicalArchitects.filter(arch => arch.id !== 'BonannoPisano');
+
+    // Return Bonanno Pisano and the remaining active classical architects
+    return [bonannoPisano, remainingArchitects];
+}
+
+
+export {getArchitects, getClassical, getActive, getBonannoPisano};
