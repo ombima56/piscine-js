37d8dfde392eb690ba8e46869d7f34be90a59c52
ombima56 <ombimahillary6@gmail.com>
2024-09-06 17:45:28 +0300

    Add solution to  function build which will create and display the amount of bricks passed as argument:
        each brick has to be created as a div and added to the page at a regular interval of 100ms.
        each brick will receive a unique id property, like the following

---
+++ build-brick-and-break.js
@@ -0,0 +1,43 @@
+let bricks = 0;
+
+const build = (n) => {
+    const intervalId = setInterval(() => {
+        // Create a new brick
+        const newDiv = document.createElement('div');
+        bricks++;
+        newDiv.id = `brick-${bricks}`;
+        newDiv.textContent = bricks;
+
+        if (bricks % 3 === 2) {
+            newDiv.setAttribute("foundation", true);
+        }
+
+        document.body.appendChild(newDiv);
+
+        // Stop the interval once the number of bricks reaches the target
+        if (bricks >= n) {
+            clearInterval(intervalId); // Clear the interval
+        }
+    }, 100); // Interval set to 100ms
+};
+
+const repair = (...ids) => {
+    ids.forEach(id => {
+        const htmlElem = document.getElementById(id);
+        if (htmlElem.hasAttribute("foundation")) {
+            htmlElem.setAttribute("data-repaired", "in progress")
+        } else {
+            htmlElem.setAttribute("data-repaired", true)
+        }
+    });
+}
+
+const destroy = () => {
+    const lastBrick = document.getElementById(`brick-${bricks}`)
+    if (bricks > 0) {
+        document.body.removeChild(lastBrick)
+        bricks--
+    }
+}
+
+export {build, repair, destroy}
