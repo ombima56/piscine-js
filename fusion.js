eb8947bac1961055ef4d82f34d54e36b3d2c0545
ombima56 <ombimahillary6@gmail.com>
2024-09-10 00:06:44 +0300

    Fix fusion function to handle type mismatches, empty strings, and undefined values
    - Improved handling of string concatenation to account for empty or falsy values
    - Ensured arrays are properly concatenated
    - Added recursive merging for objects
    - Handled type mismatches by using values from obj2 if available, otherwise falling back to obj1
    - Added logic to correctly handle cases where one of the values is undefined

--- fusion.js
+++ fusion.js
@@ -11,16 +11,19 @@ const fusion = (obj1, obj2) => {
             result[key] = [...value1, ...value2];
         } else if (typeof value1 === 'string' && typeof value2 === 'string') {
             result[key] = value1 + ' ' + value2;
-        } else if (typeof value1 === 'number' && value2 === 'number') {
+        } else if (typeof value1 === 'number' && typeof value2 === 'number') {
             result[key] = value1 + value2;
-        } else if (typeof value1 === 'object' && typeof value2 === 'object' && !Array.isArray(value1) && !Array.isArray(value2)) {
+        } else if (typeof value1 === 'object' && typeof value2 === 'object' && !Array.isArray(value1) && !Array.isArray(value2) && value1 !== null && value2 !== null) {
             result[key] = fusion(value1, value2);
+        } else if (value2 !== undefined) {
+            result[key] = value2;
         } else {
-            result[key] = value2 !== undefined ? value2 : value1
+            result[key] = value1;
         }
     }
-    return result
-}
+    return result;
+};
+
 
 console.log(fusion({ arr: [1, "2"] }, { arr: [2] })); 
 console.log(fusion({ arr: [], arr1: [5] },{ arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] }));
