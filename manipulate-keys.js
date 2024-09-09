a6fa7abca0bdbbce2f5cb6f487e277837747fd6a
ombima56 <ombimahillary6@gmail.com>
2024-09-10 01:19:26 +0300

    Implement filterKeys, mapKeys, and reduceKeys functions for grocery cart
    - Created filterKeys to filter object keys based on a provided predicate function.
    - Implemented mapKeys to transform object keys using a mapping function.
    - Developed reduceKeys to concatenate object keys into a single string, ensuring no leading commas.
    - Updated logic in reduceKeys to prevent leading commas in the output.

---
+++ manipulate-keys.js
@@ -0,0 +1,36 @@
+const filterKeys = (obj, func) => {
+    const result = {};
+
+    for ( const key of Object.keys(obj)) {
+        if (func(key)) {
+            result[key] = obj[key];
+        }
+    }
+    return result;
+}
+
+const mapKeys = (obj, func) => {
+    const result = {};
+    for ( const key of Object.keys(obj)) {
+        result[func(key)] = obj[key];
+    }
+    return result;
+}
+
+const reduceKeys = (obj, func, initialValue = '') => {
+    let accumulator = initialValue;
+
+    for (const key of Object.keys(obj)) {
+        if (accumulator == '') {
+            accumulator = key;
+        } else {
+            accumulator = func(accumulator, key);
+        }
+    }
+    return accumulator;
+}
+
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+console.log(filterKeys(nutrients, (key) => /protein/.test(key)))
+console.log(mapKeys(nutrients, (k) => `-${k}`))
+console.log(reduceKeys(nutrients, (acc, cr) =>acc.concat(', ', cr)))
