d23be85abb12427c006119c26870d53780e32b56
ombima56 <ombimahillary6@gmail.com>
2024-09-10 00:39:00 +0300

    Implement filterValues, mapValues, and reduceValues functions for object manipulation
    - Added filterValues function to filter object values based on a callback condition.
    - Added mapValues function to transform object values using a provided mapping function.
    - Added reduceValues function to aggregate object values into a single value using a reducer function.
    - Enhanced functionality to work with the provided nutrition database.

---
+++ manipulate-values.js
@@ -0,0 +1,36 @@
+const filterValues = (obj, func) => {
+
+    const result = {};
+    for ( const [key, value] of Object.entries(obj)) {
+        if (func(value)) {
+
+            result[key] = value;
+        }
+    }
+    return result;
+}
+
+const mapValues = (obj, func) => {
+    const result = {};
+
+    for ( const [key, value] of Object.entries(obj)) {
+        if (func(value)) {
+            result[key] = func(value)
+        }
+    }
+    return result
+}
+
+const reduceValues = (obj, func, initialValue) => {
+    let acc = initialValue;
+    for (const  value of Object.values(obj)) {
+        acc = func(acc, value)
+        
+    }
+    return acc
+}
+
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
+console.log(filterValues(nutrients, (nutrient) => nutrient <= 12));
+console.log(mapValues(nutrients, (v) => v + 1));
+console.log(reduceValues(nutrients, (acc, cr) => acc + cr, 0));
