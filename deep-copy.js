c2fffdff7c85f6992f46a697b6ff2d0482d6f87e
ombima56 <ombimahillary6@gmail.com>
2024-09-11 00:26:28 +0300

    Fix deepCopy function to handle RegExp objects and prevent TypeError with instanceof check

--- deep-copy.js
+++ deep-copy.js
@@ -1,39 +1,35 @@
 const deepCopy = (item) => {
     if (item === null || typeof item !== 'object') {
-      return item;
+        return item;
     }
-    
+
     // Handle Date objects
     if (item instanceof Date) {
-      return new Date(item.getTime());
+        return new Date(item.getTime());
     }
-    
+
     // Handle Array objects
     if (Array.isArray(item)) {
-      return item.map(deepCopy);
+        return item.map(deepCopy);
     }
-    
+
     // Handle RegExp objects
     if (item instanceof RegExp) {
         return new RegExp(item.source, item.flags);
     }
 
-    // Handle function object
-    if (item instanceof 'function') {
-        return item;
-    }
-    
     // Handle Object literals
     if (typeof item === 'object') {
-      const copy = {};
-      Object.keys(item).forEach(key => {
-        copy[key] = deepCopy(item[key]);
-      });
-      return copy;
+        const copy = {};
+        Object.keys(item).forEach(key => {
+            copy[key] = deepCopy(item[key]);
+        });
+        return copy;
     }
-    
+
     return Object.assign({}, item);
-}
+};
+
 
 // const original = {
 //     a: 1,
