92ef71d4efe0288694fe1cd1b38e3be39958e6eb
ombima56 <ombimahillary6@gmail.com>
2024-09-11 02:06:54 +0300

    Implement pronoun function to extract personal pronouns and their occurrences from a string

---
+++ pronoun.js
@@ -0,0 +1,30 @@
+const pronoun = (str) => {
+    const pronouns = ['i', 'you', 'he', 'she', 'it', 'they', 'we'];
+    const result = {};
+
+    const words = str.split(/\s+/);
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i].toLowerCase();
+
+        if (pronouns.includes(word)) {
+            const nextWord = words[i+1]; // Getting the word after the pronoun
+
+            if (!result[word]) {
+                result[word] = { word: [], count: 0};
+            }
+
+            if (nextWord) {
+                result[word].word.push(nextWord);
+            }
+            result[word].count++;
+        }
+    }
+    return result;
+}
+
+const ex1 = 'Using Array Destructuring, you you can iterate through objects easily.';
+console.log(pronoun(ex1)); 
+
+const ex2 = 'If he you want to buy something you  have to  pay.';
+console.log(pronoun(ex2)); 
