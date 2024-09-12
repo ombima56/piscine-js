23b08f6bddce728116eded2a50eeb098292bfe63
ombima56 <ombimahillary6@gmail.com>
2024-09-12 17:38:50 +0300

    feat: add debounce and opDebounce utility functions
    - Implemented a basic debounce function to delay execution of a function until after a specified wait time has passed since the last invocation.
    - Added opDebounce function with an option for immediate execution on the leading edge of the wait period.

---
+++ debounce.js
@@ -0,0 +1,38 @@
+const debounce = (func, wait) => {
+    let timeout;
+
+    return function(...args) {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context. args), wait);
+    };
+}
+
+const opDebounce = (func, wait, options = {leading: false}) => {
+    let timeout;
+    let lastCall = false;
+
+    return function(...args) {
+        const context = this;
+        const shouldCallNow = options.leading && !lastCall;
+        clearTimeout(timeout)
+
+        if (shouldCallNow) {
+            lastCall = true;
+            func.apply(context, args)
+        }
+
+        timeout = setTimeout(() => {
+            lastCall =false;
+            if (!options.leading) {
+                func.apply(context, args);
+            }
+        }, wait)
+    };
+}
+
+const log = debounce(() => console.log("Debounced!"), 1000);
+log();
+
+const logImmediate = opDebounce(() => console.log("Leading!"), 1000, { leading: true });
+logImmediate();
