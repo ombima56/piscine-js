c873bebfad02d2f4ae5877974860e8536469dc2e
ombima56 <ombimahillary6@gmail.com>
2024-09-17 16:43:16 +0300

    feat: create server to handle guest list updates via POST requests
    - Implemented a server that listens on port 5000
    - Supports HTTP POST requests to add/update guest information
    - Responds with appropriate status codes and JSON responses
    - Handles errors for invalid methods and server failures

---
+++ uninvited.mjs
@@ -0,0 +1,40 @@
+import http from 'node:http';
+import { writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+
+const port = 5000;
+
+const requestListener = async (req, res) => {
+    if (req.method === 'POST') {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', async () => {
+            try {
+                const guest = JSON.parse(body);
+                const fileName = join(process.cwd(), `${guest.name}.json`);
+
+                await writeFile(fileName, JSON.stringify(guest, null, 2)); 
+
+                res.writeHead(201, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(guest));
+            } catch (error) {
+                console.error('Error:', error);
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'server failed' }));
+            }
+        });
+    } else {
+        res.writeHead(405, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Method not allowed' }));
+    }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
