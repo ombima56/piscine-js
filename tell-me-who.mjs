2a10ad979cbb10785c7e2c9864893664d8211afe
ombima56 <ombimahillary6@gmail.com>
2024-09-17 14:36:20 +0300

    Fix: Correct guest name parsing and sorting logic in tell-me-who.mjs

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -2,17 +2,10 @@ import { promises as fs } from 'fs';
 import { resolve } from 'path';
 
 const formatGuestName = (filename, index) => {
-    const nameParts = filename.split(' ');
+    const nameParts = filename.split('_');
     
-    let firstname = '';
-    let lastname = '';
-
-    if (nameParts.length > 1) {
-        firstname = nameParts[0];
-        lastname = nameParts[1];
-    } else {
-        lastname = nameParts[0];
-    }
+    const firstname = nameParts[0];
+    const lastname = nameParts[1];
 
     const formattedName = `${index}. ${lastname} ${firstname}`;
     return formattedName;
@@ -21,10 +14,14 @@ const formatGuestName = (filename, index) => {
 const tellMeWho = async (directoryPath) => {
     try {
         const entries = await fs.readdir(directoryPath);
-        
+
         const sortedEntries = entries
-            .map((entry) => entry.split('.')[0]) 
-            .sort((a, b) => a.localeCompare(b)); 
+            .map((entry) => entry.split('.')[0])
+            .sort((a, b) => {
+                const [firstA, lastA] = a.split('_');
+                const [firstB, lastB] = b.split('_');
+                return lastA.localeCompare(lastB); 
+            });
 
         sortedEntries.map((entry, index) => {
             const formattedName = formatGuestName(entry, index + 1);
